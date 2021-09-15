3c7c71eaa9571a649c5afb11b82c11c76c726a2c
njfamirm <njfamirm@gmail.com>
2021-09-16 00:01:17 +0430

    dockerize postgres

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 import { createTableQuery } from "../../data/json/databaseQuery.json";
 
 export const pool = new Pool({
-  user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
   port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 });
 
 pool.connect();
